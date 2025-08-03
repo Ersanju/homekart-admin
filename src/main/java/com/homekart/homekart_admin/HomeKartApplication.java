04a485f5eb7208804115e0004d15eac888974257
Sanjay Kumar <ersanjay426@gmail.com>
2025-08-03 13:23:46 +0530

    Initial commit

---
+++ src/main/java/com/homekart/homekart_admin/HomeKartApplication.java
@@ -0,0 +1,14 @@
+package com.homekart.homekart_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeKartApplication.class, args);
+		System.out.println("Application running...");
+	}
+
+}
---
+++ src/test/java/com/homekart/homekart_admin/HomeKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homekart.homekart_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
